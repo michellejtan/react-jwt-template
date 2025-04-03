5e5b1dde3b12851f56621cc7cdab5891eca978ce
michellejtan <michelle0223@gmail.com>
2025-04-03 13:47:49 -0700

    wip: defined and exported signin service function

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,31 +4,58 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 const signUp = async (formData) => {
-  try {
-    const res = await fetch(`${BASE_URL}/sign-up`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
-    });
+    try {
+        const res = await fetch(`${BASE_URL}/sign-up`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (data.error) {
-      throw new Error(data.error);
+        if (data.error) {
+            throw new Error(data.error);
+        }
+
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            return JSON.parse(atob(data.token.split('.')[1])).payload; //1 for accessing the second value
+        }
+
+        throw new Error('Invalid response from server');
+    } catch (error) {
+        console.log(error);
+        // throw new Error(error); //don't do this, we don't want to crash
     }
+};
 
-    if (data.token) {
-      localStorage.setItem('token', data.token);
-      return JSON.parse(atob(data.token.split('.')[1])).payload;
+const signIn = async (formData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/sign-in`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
+
+        const data = await res.json();
+
+        if (data.error) {
+            throw new Error(data.error);
+        }
+
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            return JSON.parse(atob(data.token.split('.')[1])).payload;
+        }
+
+        throw new Error('Invalid response from server');
+    } catch (error) {
+        console.log(error);
+        // throw new Error(error); // don't do this, we don't want to crash
     }
-
-    throw new Error('Invalid response from server');
-  } catch (error) {
-    console.log(error);
-    throw new Error(error);
-  }
 };
 
 export {
-  signUp,
-};
+    signUp,
+    signIn
+}; // lecture don't mention export signIn
