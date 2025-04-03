a728ed8f5cb2ff32cdad2f881eae8f6dde962ac7
michellejtan <michelle0223@gmail.com>
2025-04-03 11:05:29 -0700

    wip: added userstate to provider component

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -5,11 +5,19 @@ const UserContext = createContext();  // constructor
 function UserProvider({ children }) {
     //the children prop represents each component we provide context to 
 
-  return (
-    <UserContext.Provider>
-      {children}
-    </UserContext.Provider>
-  );
+    // Create state just like you normally would in any other component
+    const [user, setUser] = useState(null);
+    // This is the user state and the setUser function that will update it!
+    // This variable name isn't special; it's just convention to use `value`.
+    const value = { user, setUser };
+
+    return (
+        <UserContext.Provider value={value}>
+            {/* The data we pass to the value prop above is now available to */}
+            {/* all the children of the UserProvider component. */}
+            {children}
+        </UserContext.Provider>
+    );
 };
 
-export { UserProvider };
+export { UserProvider, UserContext };
