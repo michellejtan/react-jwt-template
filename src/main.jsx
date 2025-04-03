27e2e5e041fe32aa7810b76411bdf73097c75f07
michellejtan <michelle0223@gmail.com>
2025-04-03 10:58:42 -0700

    wip: configs userprovider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { UserProvider } from './contexts/UserContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
@@ -9,7 +10,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      {/* Wrap the UserProvider around the App */}
+      <UserProvider>
+        <App />
+      </UserProvider>    </BrowserRouter>
   </StrictMode>,
 )
