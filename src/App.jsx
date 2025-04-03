c52c8c3a3088bc25eede70fa232af2a1713ad007
michellejtan <michelle0223@gmail.com>
2025-04-03 10:22:50 -0700

    wip: defines first route rendering signupform

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Routes, Route } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
-
+import SignUpForm from './components/SignUpForm/SignUpForm';
 const App = () => {
   
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
-    </>
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>    </>
   );
 };
 
