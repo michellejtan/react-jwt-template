8ecddd552d64a010d09b110886d12b7b7a6b3480
michellejtan <michelle0223@gmail.com>
2025-04-03 11:01:22 -0700

    wip: defines first route rendering signupform

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,77 +1,85 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { signUp } from '../../services/authService';
+
 
 const SignUpForm = () => {
-  const navigate = useNavigate();
-  const [message, setMessage] = useState('');
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    passwordConf: '',
-  });
+    const navigate = useNavigate();
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        passwordConf: '',
+    });
 
     // destructure our formData into individual vars 
-  const { username, password, passwordConf } = formData;
+    const { username, password, passwordConf } = formData;
 
-  const handleChange = (evt) => {
-    setMessage(''); // thinking backward
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
-  };
+    const handleChange = (evt) => {
+        setMessage(''); // thinking backward
+        setFormData({ ...formData, [evt.target.name]: evt.target.value });
+    };
 
-  const handleSubmit = async (evt) => {
-    evt.preventDefault();
-    console.log(formData); // this line will print the form data to the console
-  };
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        // console.log(formData); // this line will print the form data to the console
+        try {
+            const newUser = await signUp(formData);
+            console.log(newUser);
+        } catch (error) {
+            setMessage(error.message);
+        }
+    };
 
-  const isFormInvalid = () => {
-    return !(username && password && password === passwordConf);
-  };
+    const isFormInvalid = () => {
+        return !(username && password && password === passwordConf);
+    };
 
-  return (
-    <main>
-      <h1>Sign Up</h1>
-      <p>{message}</p>
-      <form onSubmit={handleSubmit}>
-        <div> {/* label group, to do css stuff*/}
-          <label htmlFor='username'>Username:</label>
-          <input
-            type='text'
-            id='name'
-            value={username}
-            name='username'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='password'
-            value={password}
-            name='password'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='passwordConf'>Confirm Password:</label>{/* dan prefer value, name, the same, lecture is "confirm*/}
-          <input
-            type='password'
-            id='confirm'
-            value={passwordConf}
-            name='passwordConf'
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div> {/* div for futrue css */}
-          <button disabled={isFormInvalid()}>Sign Up</button> {/* disabled - boolean */}
-          <button onClick={() => navigate('/')}>Cancel</button> {/* cancel and navigate back to home */}
-        </div>
-      </form>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Sign Up</h1>
+            <p>{message}</p>
+            <form onSubmit={handleSubmit}>
+                <div> {/* label group, to do css stuff*/}
+                    <label htmlFor='username'>Username:</label>
+                    <input
+                        type='text'
+                        id='name'
+                        value={username}
+                        name='username'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password:</label>
+                    <input
+                        type='password'
+                        id='password'
+                        value={password}
+                        name='password'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor='passwordConf'>Confirm Password:</label>{/* dan prefer value, name, the same, lecture is "confirm*/}
+                    <input
+                        type='password'
+                        id='confirm'
+                        value={passwordConf}
+                        name='passwordConf'
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div> {/* div for futrue css */}
+                    <button disabled={isFormInvalid()}>Sign Up</button> {/* disabled - boolean */}
+                    <button onClick={() => navigate('/')}>Cancel</button> {/* cancel and navigate back to home */}
+                </div>
+            </form>
+        </main>
+    );
 };
 
 export default SignUpForm;
