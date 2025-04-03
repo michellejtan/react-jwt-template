1427c402d116a390bc6fd64a4408f48ce2d61a20
michellejtan <michelle0223@gmail.com>
2025-04-03 11:42:25 -0700

    wip: intialized user state from localstorage

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -2,11 +2,21 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();  // constructor 
 
+// Add the new getUserFromToken function
+const getUserFromToken = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) return null;
+
+    return JSON.parse(atob(token.split('.')[1])).payload;
+};
+
 function UserProvider({ children }) {
     //the children prop represents each component we provide context to 
 
-    // Create state just like you normally would in any other component
-    const [user, setUser] = useState(null);
+    // (1)Create state just like you normally would in any other component
+    // (2) call getUserFromToken() to get our initial user state, instead of null now, reading from localstorage
+    const [user, setUser] = useState(getUserFromToken());
     // This is the user state and the setUser function that will update it!
     // This variable name isn't special; it's just convention to use `value`.
     const value = { user, setUser };
